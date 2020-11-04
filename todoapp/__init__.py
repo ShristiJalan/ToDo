0e549b314bbc480729c85e1c95c3a65f52737f1d
ShristiJalan <shristijalan1010@gmail.com>
2020-11-04 22:56:43 +0530

    done wih html file

--- todoapp/__init__.py
+++ todoapp/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask 
+from flask import Flask
+
+from .main.routes import main
 
 from .extensions import mongo
 
@@ -8,5 +10,7 @@ def create_app():
     app.config['MONGO_URI'] = 'mongodb+srv://Shristi_Cluster:Password1@cluster0.pevmz.mongodb.net/mydb?retryWrites=true&w=majority'
     
     mongo.init_app(app)
+    
+    app.register_blueprint(main)
     
     return app
--- todoapp/main/routes.py
+++ todoapp/main/routes.py
@@ -1,7 +1,43 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+from todoapp.extensions import mongo
 
 main = Blueprint('main', __name__)
 
+
+
 @main.route('/')
 def index():
-    return render_template('index.html')
+    todos_collection = mongo.db.todos
+    todos = todos_collection.find()
+    return render_template('index.html', todos=todos)
+
+@main.route('/add_todo', methods=['POST'])
+def add_todo():
+    todos_collection = mongo.db.todos
+    todo_item = request.form.get('add-todo')
+    todos_collection.insert_one({'text' : todo_item, 'complete' : False})
+    return redirect(url_for('main.index'))
+    
+@main.route('/complete_todo/<oid>')
+def complete_todo(oid):
+    todo_collecion = mongo.db.todos
+    todo_item = todos_collection.find_one({'_id' : ObjectId(oid)})
+    todo_item['complete'] = True
+    todos_collection.save(todo_item)
+    return redirect(url_for('main.index'))
+
+@main.route('/delete_completed')
+def delete_completed():
+    todo_collecion = mongo.db.todos
+    todos_collection.delete_many({'complete' : True})
+    return redirect(url_for('main.index'))
+
+@main.route('/delete_all')
+def delete_all():
+    todo_collecion = mongo.db.todos
+    todos_collection.delete_many({})
+    return redirect(url_for('main.index'))
+    
